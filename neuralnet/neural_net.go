f42beae92c69867de2deadb3608f0bf09a9538ec
Evis Drenova <evis@usenucleus.cloud>
2024-01-14 01:43:35 -0800

    implements leaky_relu, sigmoid and tanh

--- neuralnet/neural_net.go
+++ neuralnet/neural_net.go
@@ -1,16 +1,54 @@
 package nn
 
-import "gotorch/tensor"
+import (
+	"gotorch/tensor"
+	"math"
+)
 
-// implements a ReLu activation function on each element in a tensor
-func ReLu(data *tensor.Tensor) []float64 {
-	result := make([]float64, len(data.Data))
-	for i, val := range data.Data {
+const leaky_relu_constant = 0.01
+
+// implements a ReLu activation function on each element in a tensor where f(x) = max(0,x)
+func ReLu(t *tensor.Tensor) *tensor.Tensor {
+	result := make([]float64, len(t.Data))
+	for i, val := range t.Data {
 		if val < 0 {
 			result[i] = 0
 		} else {
 			result[i] = val
 		}
 	}
-	return result
+	return &tensor.Tensor{Data: result, Shape: t.Shape}
+}
+
+// implements leaky_relu which is like relu but instead of zero'ing out anything less than 0, we multiply it by a small constant
+func Leaky_ReLu(t *tensor.Tensor) *tensor.Tensor {
+	result := make([]float64, len(t.Data))
+	for i, val := range t.Data {
+		if val < 0 {
+			result[i] = val * leaky_relu_constant
+		} else {
+			result[i] = val
+		}
+	}
+	return &tensor.Tensor{Data: result, Shape: t.Shape}
+}
+
+func Sigmoid(t *tensor.Tensor) *tensor.Tensor {
+
+	result := make([]float64, len(t.Data))
+	for i := range t.Data {
+		result[i] = 1 / (1 + math.Exp(-t.Data[i]))
+	}
+	return &tensor.Tensor{Data: result, Shape: t.Shape}
+
+}
+
+func Tanh(t *tensor.Tensor) *tensor.Tensor {
+
+	result := make([]float64, len(t.Data))
+	for i := range t.Data {
+		result[i] = (math.Exp(t.Data[i]-math.Exp(-t.Data[i])) / (math.Exp(t.Data[i]) + math.Exp(-t.Data[i])))
+	}
+	return &tensor.Tensor{Data: result, Shape: t.Shape}
+
 }
--- neuralnet/neural_net_test.go
+++ neuralnet/neural_net_test.go
@@ -2,6 +2,7 @@ package nn
 
 import (
 	"gotorch/tensor"
+	"math"
 	"testing"
 )
 
@@ -13,10 +14,65 @@ func Test_ReLu(t *testing.T) {
 
 	expected := []float64{2, 0, 4, 0}
 
-	for i, _ := range result {
-		if result[i] != expected[i] {
-			t.Errorf("ReLu function not applied correctly, expected: %v, got: %v", expected[i], result[i])
+	for i := range result.Data {
+		if result.Data[i] != expected[i] {
+			t.Errorf("ReLu function not applied correctly, expected: %v, got: %v", expected[i], result.Data[i])
 		}
 	}
 
+}
+
+func Test_Leaky_ReLu(t *testing.T) {
+
+	tensor := tensor.NewTensor([]float64{2, -3, 4, -5})
+
+	result := Leaky_ReLu(tensor)
+
+	expected := []float64{2, -0.03, 4, -0.05}
+
+	for i := range result.Data {
+		if result.Data[i] != expected[i] {
+			t.Errorf("ReLu function not applied correctly, expected: %v, got: %v", expected[i], result.Data[i])
+		}
+	}
+}
+
+func Test_Sigmoid(t *testing.T) {
+
+	tensor := tensor.NewTensor([]float64{2, -3, 4, -5})
+
+	result := Sigmoid(tensor)
+
+	expected := []float64{
+		1 / (1 + math.Exp(-2)),
+		1 / (1 + math.Exp(3)),
+		1 / (1 + math.Exp(-4)),
+		1 / (1 + math.Exp(5)),
+	}
+
+	for i := range result.Data {
+		if math.Abs(result.Data[i]-expected[i]) > 1e-6 { // using a small threshold for floating point comparison
+			t.Errorf("Sigmoid function not applied correctly at index %d, expected: %v, got: %v", i, expected[i], result.Data[i])
+		}
+	}
+}
+
+func Test_Tanh(t *testing.T) {
+
+	tensor := tensor.NewTensor([]float64{2, -3, 4, -5})
+
+	result := Tanh(tensor)
+
+	expected := []float64{
+		(math.Exp(2-math.Exp(-2)) / (math.Exp(2) + math.Exp(-2))),
+		(math.Exp(-3-math.Exp(3)) / (math.Exp(-3) + math.Exp(3))),
+		(math.Exp(4-math.Exp(-4)) / (math.Exp(4) + math.Exp(-4))),
+		(math.Exp(-5-math.Exp(5)) / (math.Exp(-5) + math.Exp(5))),
+	}
+
+	for i := range result.Data {
+		if math.Abs(result.Data[i]-expected[i]) > 1e-6 { // using a small threshold for floating point comparison
+			t.Errorf("Sigmoid function not applied correctly at index %d, expected: %v, got: %v", i, expected[i], result.Data[i])
+		}
+	}
 }
